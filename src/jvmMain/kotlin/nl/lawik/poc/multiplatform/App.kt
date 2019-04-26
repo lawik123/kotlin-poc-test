acf361a8f23ed90cbd911efe533d15fa2eb5c48d
Lawik <428302@student.saxion.nl>
2019-04-26 16:14:35 +0200

    removed unused code

--- src/jvmMain/kotlin/nl/lawik/poc/multiplatform/App.kt
+++ src/jvmMain/kotlin/nl/lawik/poc/multiplatform/App.kt
@@ -1,16 +1,8 @@
 package nl.lawik.poc.multiplatform
 
-import nl.lawik.poc.multiplatform.dto.PersonDTO
 import nl.lawik.poc.multiplatform.endpoint.API_PATH
 import javax.ws.rs.ApplicationPath
 import javax.ws.rs.core.Application
 
 @ApplicationPath(API_PATH)
 class App : Application()
-
-fun main() {
-    val person = PersonDTO(null, "", 24)
-    if (person is Validateable<*>) {
-        person.validateCreate()
-    }
-}
